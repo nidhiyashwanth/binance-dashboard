61ae6941ea671ef84065bcbb2525df18bb25431e
nidhiyashwanth <nidhiyashwanth007@gmail.com>
2024-01-24 13:28:43 +0530

    some mior fixes

--- src/features/dashboard/Dashboard.js
+++ src/features/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setCoins } from "./dashboardSlice";
-import { socket } from "../../api/binance-websocket"; // Import the socket directly
+import { socket } from "../../api/binance-websocket";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
